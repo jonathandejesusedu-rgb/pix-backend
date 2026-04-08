e4c691afb11dd392fec4c119c398e77fe0a4067e
jonathandejesusedu-rgb <jonathandejesusedu@gmail.com>
2026-04-08 20:31:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor rodando");
+});
+
 app.get("/pix", async (req, res) => {
   try {
     const response = await axios.post(
       "https://api.paradisepags.com/v1/transactions",
       {
-        amount: 50.00,
-        payment_method: "pix",
+        amount: 5000,
+        paymentMethod: "pix",
         customer: {
           name: "Cliente Teste",
-          email: "cliente@email.com",
-          document: "12345678900"
+          email: "cliente@email.com"
         }
       },
       {
         headers: {
-          "X-API-Key": "sk_62969b67edd2adec815b1b87440cca0414c15fc589f60ca54aafdc82099610d9",
-          "Content-Type": "application/json"
+          "X-API-Key": "sk_62969b67edd2adec815b1b87440cca0414c15fc589f60ca54aafdc82099610d9"
         }
       }
     );
 
     res.json(response.data);
   } catch (error) {
-    console.log(error.response?.data || error.message);
+    console.log(error.message);
     res.send("Erro ao gerar Pix");
   }
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
